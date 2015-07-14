18408cac4ee31b26540823437b098ec6a7b16146
ZZEXX0 <mrbbot@gmail.com>
2015-07-14 18:38:41 +0100

    Mod class created

---
+++ src/main/java/com/mrbbot/magicalbooks/MagicalBooks.java
@@ -0,0 +1,29 @@
+package com.mrbbot.magicalbooks;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MagicalBooks", name = "Magical Books", version = "1.7.10-1.0")
+public class MagicalBooks {
+
+    @Mod.Instance
+    public static MagicalBooks instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
